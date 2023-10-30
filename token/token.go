4d45c53ffb8bb13f5bbd27616e4dc751d188f6d1
jc <46619361+juancwu@users.noreply.github.com>
2023-10-29 23:24:34 -0400

    format token.go

--- token/token.go
+++ token/token.go
@@ -3,45 +3,45 @@ package token
 type TokenType string
 
 type Token struct {
-  Type TokenType
-  Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-  ILLEGAL = "ILLEGAL"
-  EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  // Identifiers + Literals
-  IDENT = "IDENT"
-  INT = "INT"
+	// Identifiers + Literals
+	IDENT = "IDENT"
+	INT   = "INT"
 
-  // Operators
-  ASSIGN = "="
-  PLUS = "+"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
 
-  // Delimeters
-  COMMA = ","
-  SEMICOLON = ";"
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-  LPAREN = "("
-  RPAREN = ")"
-  LBRACE = "{"
-  RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-  // Keywords
-  FUNCTION = "FUNCTION"
-  LET = "LET"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
 
 var keywords = map[string]TokenType{
-    "fn": FUNCTION,
-    "let": LET,
+	"fn":  FUNCTION,
+	"let": LET,
 }
 
 func LookupIdent(ident string) TokenType {
-    if tok, ok := keywords[ident]; ok {
-        return tok
-    }
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
 
-    return IDENT
+	return IDENT
 }
