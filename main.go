3b18f30ab04f7389331c1c24013ae1ea80935a7d
jc <46619361+juancwu@users.noreply.github.com>
2024-01-11 08:52:10 -0500

    format main.go

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/user"
-    
-    "github.com/juancwu/interpreter-in-go/repl"
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/juancwu/interpreter-in-go/repl"
 )
 
 func main() {
-    user, err := user.Current()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-    fmt.Printf("Feel free to type in commands\n")
-    repl.Start(os.Stdin, os.Stdout)
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
